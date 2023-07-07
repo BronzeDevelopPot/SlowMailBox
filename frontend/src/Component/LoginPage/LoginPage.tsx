3f4542c61a562f6a4ca8d06759877628926d8c14
areumH <94692566+areumH@users.noreply.github.com>
2023-07-07 23:59:52 +0900

    feat: [Client] #2 Create login page

--- frontend/src/Component/LoginPage/LoginPage.tsx
+++ frontend/src/Component/LoginPage/LoginPage.tsx
@@ -1,1 +1,21 @@
-// 
+import styles from "../../Styles/_LoginPage.module.scss";
+
+const LoginPage = () => {
+  return (
+    <div className={styles.loginPage}>
+      <div className={styles.title}> 추억의 느린 우체통 </div>
+
+      <div className={styles.mailBox} />
+
+      <div className={styles.signUpTitle}> 느린 우체통 만들기</div>
+      <a>
+        {/* href로 카카오 연결 */}
+        <div className={styles.kakaoLoginButton}>
+          <img src="/src/Assets/kakaologin.png"></img>
+        </div>
+      </a>
+    </div>
+  );
+};
+
+export default LoginPage;
