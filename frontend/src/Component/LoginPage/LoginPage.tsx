f201640e1bd58c1abc781ac1e0c9ded645273ca7
Moon SoHee <qwerpoiu91@naver.com>
2023-07-09 14:26:15 +0900

    fix: [Client] Fix the login page scss

--- frontend/src/Component/LoginPage/LoginPage.tsx
+++ frontend/src/Component/LoginPage/LoginPage.tsx
@@ -3,15 +3,15 @@ import styles from "../../Styles/_loginPage.module.scss";
 
 const LoginPage = () => {
   return (
-    <div className={styles.loginPage}>
+    <div className={styles.login_page}>
       <div className={styles.title}> 추억의 느린 우체통 </div>
 
-      <div className={styles.mailBox} />
+      <div className={styles.mail_box} />
 
-      <div className={styles.signUpTitle}> 느린 우체통 만들기</div>
+      <div className={styles.sign_up_title}> 느린 우체통 만들기</div>
       <a>
         {/* href로 카카오 연결 */}
-        <div className={styles.kakaoLoginButton}>
+        <div className={styles.kakao_login_button}>
           <img src="/src/Assets/kakaologin.png"></img>
         </div>
       </a>
