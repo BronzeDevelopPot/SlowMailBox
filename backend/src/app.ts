4a6e146c7f3153aae6e9a5f9cce4673ceafda948
Moon SoHee <106158901+ccconac@users.noreply.github.com>
2023-07-17 14:55:25 +0900

    feat: [Server] #8 Setting Firebase

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,23 @@
-import express from "express";
-
+const admin = require("firebase-admin");
+const firestore = require("firebase-admin/firestore");
+const serviceAccount = require("../config");
 const cors = require("cors");
-const app = express();
+const app = require("express");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const database = firestore.getFirestore();
+
+test();
+
+async function test() {
+    database.collection("test").doc("test").set({
+        first:"first",
+        second: "second"
+    });
+}
 
 app.use(cors());
 
