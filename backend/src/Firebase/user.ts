34602f8a949c8dca21ee2cd8557a7affbf0372e8
Moon SoHee <106158901+ccconac@users.noreply.github.com>
2023-07-26 11:33:41 +0900

    feat: [Server] #11 Increase ‘totalUser’ in DB upon sign up

--- backend/src/Firebase/user.ts
+++ backend/src/Firebase/user.ts
@@ -1,10 +1,21 @@
-import { doc, setDoc } from "firebase/firestore";
+import { doc, increment, setDoc, getDoc, updateDoc, collection } from "firebase/firestore";
 import { database } from "../Config/firebaseConfig";
 
 export const setUsers = async (userData: any) => {
-    await setDoc(doc(database, "slowmailbox", "users"), {
+    const userInfo = {
         _id: userData.id,
         email: userData.kakao_account.email,
         name: userData.kakao_account.profile.nickname
-    });
-} 
+    }
+    
+    const userRef = doc(collection(database, "slowmailbox", "user", "users"), `${userInfo._id}`);
+    const userDoc = await getDoc(userRef);
+
+    if (!userDoc.exists()) {
+        await updateDoc(doc(database, "slowmailbox", "counter"), {
+            totalUser: increment(1)
+        });
+
+        await setDoc(userRef, userInfo);
+    }
+}
