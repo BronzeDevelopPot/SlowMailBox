a769188f815ee82b275215b70875e054568f1f3d
Moon SoHee <106158901+ccconac@users.noreply.github.com>
2023-07-31 09:43:13 +0900

    design: [Client] Change margin of Main and Login Page

--- frontend/src/Component/LoginPage/LoginPage.tsx
+++ frontend/src/Component/LoginPage/LoginPage.tsx
@@ -1,4 +1,5 @@
 import { KAKAO_AUTH_URL } from "./Auth";
+import { Link } from "react-router-dom";
 import styles from "../../Styles/_loginPage.module.scss";
 
 const LoginPage = () => {
@@ -12,6 +13,10 @@ const LoginPage = () => {
           <img src="/src/Assets/kakaologin.png" />
         </div>
       </a>
+      {/* 임시 main page button */}
+      <Link to="/main">
+        <button>main page</button>
+      </Link>
     </div>
   );
 };
