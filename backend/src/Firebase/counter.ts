a3de7119139bd8f63e2c8374c07cf2bf5a49eff1
ccconac <qwerpoiu91@naver.com>
2023-08-11 22:25:14 +0900

    fix: [Server] Remove totalLetter field (#12)

--- backend/src/Firebase/counter.ts
+++
@@ -1,7 +0,0 @@
-import { database } from "../Config/firebaseConfig";
-import { doc, updateDoc, increment } from "firebase/firestore";
-
-export const setTotalLetter = async () => {
-  const counterRef = doc(database, "slowmailbox", "counter");
-  await updateDoc(counterRef, { totalLetter: increment(1) });
-};
--- backend/src/Firebase/mailbox.ts
+++ backend/src/Firebase/mailbox.ts
@@ -1,6 +1,5 @@
 import { collection, doc, setDoc, getDoc } from "firebase/firestore";
 import { database } from "../Config/firebaseConfig";
-import { setTotalLetter } from "./counter";
 
 export const saveLetter = async (reqBody: any) => {
   try {
@@ -14,7 +13,7 @@ export const saveLetter = async (reqBody: any) => {
       monthDif: monthDif,
     };
 
-    const letterRef = doc(collection(database, "slowmailbox", "mailbox", "letters"), `${to}`);
+    const letterRef = doc(collection(database, "slowmailbox", "mailbox", "letters"), to);
 
     /* 
      letterRef 경로에서 현재 letter 배열을 가지고 오고, 현재 배열이 undefiend라면 초기값을 빈 배열([])로 설정 
@@ -24,18 +23,12 @@ export const saveLetter = async (reqBody: any) => {
     const currentLetter = getLetter.data()?.letter || [];
     const newLetter = [...currentLetter, letterData];
 
-    /* 유저가 받은 편지 개수를 계산하기 위해 배열의 길이 저장 */
-    const letterCount = newLetter.length;
-
     /* letterRef 경로에 letter data 저장 */
     await setDoc(letterRef, {
       userID: toId,
       userName: to,
       letter: newLetter,
-      totalLetter: letterCount,
     });
-
-    await setTotalLetter();
   } catch (e) {
     console.log(`편지 저장 실패: ${e}`);
   }
@@ -48,4 +41,4 @@ export const getLetter = async (reqParams: any) => {
   const letterDoc = await getDoc(letterRef);
 
   return letterDoc;
-}
+};
