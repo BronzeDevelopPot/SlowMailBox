8e0bc3dd525c3f71c15aaf9cebd37ca7aa2e6ab0
areumH <dkfmal0109@gmail.com>
2023-08-17 22:01:24 +0900

    feat: [Client] #21 Save letter upon push button

--- frontend/src/Component/ArrivePage/MailModalPage/MailModalPage.tsx
+++ frontend/src/Component/ArrivePage/MailModalPage/MailModalPage.tsx
@@ -1,22 +1,44 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+import domtoimage from "dom-to-image";
+import { saveAs } from "file-saver";
 import styles from "../../../Styles/_mailModalPage.module.scss";
 
 const MailModalPage = (_props: any) => {
   const [from] = useState<string>("from : " + _props.fromName);
   const [letter] = useState<string>(_props.letterText);
+  const captureRef = useRef(null);
+
+  const onHandleDownload = () => {
+    const element = captureRef.current;
+
+    if (element) {
+      const option = {
+        style: {
+          width: "440px",
+          height: "700px",
+        },
+      };
+      domtoimage.toBlob(element, option).then((blob) => {
+        saveAs(blob, `letterfrom${from}.png`);
+      });
+    }
+  };
 
   return (
     <div className={styles.mail_modal_page}>
-      <div className={styles.mail_modal_container}>
-        <div className={styles.close_button} onClick={_props.onHandleModal}>X</div>
+      <div ref={captureRef} className={styles.mail_modal_container}>
         <div className={styles.letter_style}>
           {from} <br />
           {letter}
         </div>
-        <button>저장</button>
       </div>
+      <div className={styles.close_button} onClick={_props.onHandleModal}>
+        X
+      </div>
+      <button onClick={onHandleDownload}>저장</button>
     </div>
   );
 };
 
 export default MailModalPage;
+
